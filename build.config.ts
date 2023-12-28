7a544b44834ebc3da6f44e5190ee6b28bee8b352
Pooya Parsa <pooya@pi0.io>
2023-12-28 23:25:17 +0100

    build: export commonjs

--- build.config.ts
+++ build.config.ts
@@ -2,6 +2,7 @@ import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   declaration: true,
+  rollup: { emitCJS: true },
   entries: ["src/plugin", "src/tools"],
   externals: ["unwasm", "rollup"],
 });
