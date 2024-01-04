30ba936f6b9e5b8797e38e7b24e747d10027b66e
Pooya Parsa <pooya@pi0.io>
2024-01-04 19:03:57 +0100

    linnk to issue

--- test/fixture/dynamic-import.mjs
+++ test/fixture/dynamic-import.mjs
@@ -1,11 +1,12 @@
-const { rand } = await import("@fixture/wasm/rand.wasm");
-const { sum } = await import("@fixture/wasm/sum.wasm");
+export async function test() {
+  // Avoid top-level await because of miniflare curren't limitation.
+  // https://github.com/cloudflare/miniflare/issues/753
+  const { rand } = await import("@fixture/wasm/rand.wasm");
+  const { sum } = await import("@fixture/wasm/sum.wasm");
 
-export function test() {
-  // Seems a bug with Miniflare and two async functions...
-  // if (sum(1, 2) !== 3) {
-  //   return "FALED: sum";
-  // }
+  if (sum(1, 2) !== 3) {
+    return "FALED: sum";
+  }
   if (!(rand(0, 1000) > 0)) {
     return "FALED: rand";
   }
